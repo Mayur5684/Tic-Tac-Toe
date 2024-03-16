4932635c80ad6f4ce68d459e623181df4dca455a
Mayur5684 <163518811+Mayur5684@users.noreply.github.com>
2024-03-16 23:21:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,108 @@
-// Get the DOM elements and initialize the game
-const input = document.querySelector("input"),
-  guess = document.querySelector(".guess"),
-  checkButton = document.querySelector("button"),
-  remainChances = document.querySelector(".chances");
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+//Winning Pattern Array
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+//Player 'X' plays first
+let xTurn = true;
+let count = 0;
 
-// Set the focus on input field
-input.focus();
+//Disable All Buttons
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  //enable popup
+  popupRef.classList.remove("hide");
+};
 
-let randomNum = Math.floor(Math.random() * 100);
-chance = 10;
+//Enable all buttons (For New Game and Restart)
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  //disable popup
+  popupRef.classList.add("hide");
+};
 
-// Listen for the click event on the check button
-checkButton.addEventListener("click", () => {
-  // Decrement the chance variable on every click
-  chance--;
-  // Get the value from the input field
-  let inputValue = input.value;
-  // Check if the input value is equal to the random number
-  if (inputValue == randomNum) {
-    // Update guessed number, disable input, check button text and color.
-    [guess.textContent, input.disabled] = ["Congratulations", true];
-    [checkButton.textContent, guess.style.color] = ["Replay", "#333"];
-    //Check if input value is > random number and within 1-99 range.
-  } else if (inputValue > randomNum && inputValue < 100) {
-    // Update the guess text and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your guess is high", chance];
-    guess.style.color = "#333";
-    //Check if input value is < random number and within 1-99 range.
-  } else if (inputValue < randomNum && inputValue > 0) {
-    // Update the guessed number text and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your guess is low", chance];
-    guess.style.color = "#333";
-    // If the input value is not within the range of 1 to 99
+//This function is executed when a player wins
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
   } else {
-    // Update the guessed number text, color and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your number is invalid", chance];
-    guess.style.color = "#DE0611";
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
   }
-  // Check if the chance is zero
-  if (chance == 0) {
-    //Update check button, disable input, and clear input value.
-    // Update guessed number text and color to indicate user loss.
-    [checkButton.textContent, input.disabled, inputValue] = ["Replay", true, ""];
-    [guess.textContent, guess.style.color] = ["You lost the game", "#DE0611"];
-  }
-  if (chance < 0) {
-    window.location.reload();
-  }
+};
+
+//Function for draw
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+
+//New Game
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
 });
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+//Win Logic
+const winChecker = () => {
+  //Loop through all win patterns
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    //Check if elements are filled
+    //If 3 empty elements are same and would give win as would
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        //If all 3 buttons have same values then pass the value to winFunction
+        winFunction(element1);
+      }
+    }
+  }
+};
+
+//Display X/O on click
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      //Display X
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+      //Display Y
+      element.innerText = "O";
+      element.disabled = true;
+    }
+    //Increment count on each click
+    count += 1;
+    if (count == 9) {
+      drawFunction();
+    }
+    //Check for win on every click
+    winChecker();
+  });
+});
+//Enable Buttons and disable popup on page load
+window.onload = enableButtons;
